b6de77aa8b86678c9bdd8a24331e46cf06271ee7
Joe Pereira <joepereira89@gmail.com>
2018-12-09 17:08:49 -0600

    functions, finally got around to uploading

--- R/ezr_groupby_metric.R
+++ R/ezr_groupby_metric.R
@@ -14,7 +14,7 @@
 #'
 #' @examples myfunct_groupby_metric(mtcars, 'vs')
 
-ezr.groupby_metric = function(dataset, groupby_fields, metric="mean(., na.rm=TRUE)", numeric_fields_only=TRUE, num_breaks = 5){
+ezr.groupby_metric = function(dataset, groupby_fields, metric="mean(., na.rm=TRUE)", numeric_fields_only=TRUE, num_breaks = 5, return_pct_change=TRUE){
 
     if (numeric_fields_only==TRUE ){
         groupby_data = dataset %>% group_by(!!!rlang::syms(groupby_fields)) %>% select_if(base::is.numeric)
@@ -23,19 +23,19 @@ ezr.groupby_metric = function(dataset, groupby_fields, metric="mean(., na.rm=TRU
     }
 
 
-    if (dataset[[groupby_fields]] %>% base::is.numeric()==TRUE){
+    if (dataset %>% dplyr::select(groupby_fields) %>% base::is.numeric()==TRUE){
 
        df_records = nrow(dataset)
         breaks = classInt::classIntervals(dataset[[groupby_fields]],style = 'quantile',n = num_breaks)$brks
         breaks =cut(dataset[[groupby_fields]],breaks = breaks, include.lowest = TRUE, ordered_result = TRUE,dig.lab=10 )
 
         dataset[[groupby_fields]] = breaks
-        groupby_data = dataset %>% group_by(!!!rlang::syms(groupby_fields))
+        groupby_data = dataset %>% dplyr::group_by(!!!rlang::syms(groupby_fields))
 
         if (numeric_fields_only ==TRUE ){
-            bad_names = dataset  %>% select_if(purrr::negate(base::is.numeric)) %>% names()
+            bad_names = dataset  %>% dplyr::select_if(purrr::negate(base::is.numeric)) %>% names()
             bad_names =  setdiff(bad_names ,groupby_fields)
-            groupby_data =groupby_data %>% dplyr::select(-one_of(bad_names))
+            groupby_data = groupby_data %>% dplyr::select(-one_of(bad_names))
         }
     }
 
@@ -45,23 +45,39 @@ ezr.groupby_metric = function(dataset, groupby_fields, metric="mean(., na.rm=TRU
 
     all_data = groupby_data %>% ungroup() %>% dplyr::select(-one_of(groupby_fields)) %>% summarise_all(
         .funs = funs(!!rlang::parse_expr(metric))  )
+    for(each_var in groupby_fields){
+        all_data[[each_var]] = as.factor('all_data')
+    }
+
+
+
+
 
     result = bind_rows(groupby_data, all_data)
 
-    result = result %>% mutate_at(.vars=vars(groupby_fields), .funs = funs(replace_na(., 'ALL')))
 
     # cleaning up NAs in binded data.
 
 
-    for (each_var in groupby_fields){
-        result = result %>% mutate(
-            !!each_var := ifelse(is.na(!!rlang::sym(each_var))==TRUE,'all_data',!!rlang::sym(each_var))
-        )
+    if(return_pct_change==TRUE){
+
+        result = result %>% ungroup() %>% mutate_at(.vars = vars(groupby_fields), base::factor)
+        reference = result %>% ungroup() %>% dplyr::slice(1)
+
+
+        pct_change = result/ reference  %>% dplyr::slice(rep(1:n(), each=nrow(result)))
+        pct_change = bind_cols(result %>% select(groupby_fields), pct_change %>% select(-groupby_fields))
+
+        non_grouping_vars = setdiff(names(pct_change), groupby_fields)
+        pct_change = pct_change %>% mutate_at(.vars = vars(non_grouping_vars), .funs=funs(round(.,2)))
+
+        print('Returning a list of two dataframes. $metrics and $pct_change, which is relative to first row')
+
+        return(result=list(metrics=result, pct_change=pct_change))
     }
+
 
 
     return(result)
 }
-
-
 
